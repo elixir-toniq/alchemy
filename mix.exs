5ba17c740fc8df32c6cffa326344f7d841074834
Chris Keathley <c@keathley.io>
2019-04-24 11:27:02 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Alchemy.Mixfile do
     [
       app: :alchemy,
       description: "Perform experiments in production",
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.7",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
